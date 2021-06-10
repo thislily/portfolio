6bad0eb3b82416e16d238be99fdb8f1ae3a32aaf
lily <lilyclairewatson@gmail.com>
2021-06-10 22:41:56 +0200

    now with functionality!

---
+++ background.js
@@ -0,0 +1,36 @@
+
+//box one
+
+let boxOne = document.getElementById('box1')
+
+boxOne.onmouseover = function() {
+    boxOne.style.backgroundImage = "url('beefybuffboys.jpg')"
+};
+
+boxOne.onmouseout = function() {
+    boxOne.style.backgroundImage = "none";
+};
+
+//box two
+
+let boxTwo = document.getElementById('box2')
+
+boxTwo.onmouseover = function() {
+    boxTwo.style.backgroundImage = "url('floopydoops.jpg')"
+};
+
+boxTwo.onmouseout = function() {
+    boxTwo.style.backgroundImage = "none"
+};
+
+//box three
+
+let boxThree = document.getElementById('box3')
+
+boxThree.onmouseover = function() {
+    boxThree.style.backgroundImage = "url('chillout.jpg')"
+};
+
+boxThree.onmouseout = function() {
+    boxThree.style.backgroundImage = "none"
+};
